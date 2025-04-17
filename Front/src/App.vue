5c2ef0d0ddccceba449fd376dd10f9db011ed68b
Alexandre VIILANI <alexandre.villani@edu.univ-fcomte.fr>
2025-04-17 14:29:45 +0200

    modif pour render.com

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,19 +1,31 @@
+<!-- src/views/Activites.vue -->
 <template>
-  <div id="app">
-    <Navbar v-if="!route.meta.hideNavbar" />
-    <RouterView />
-    <Footer v-if="!route.meta.hideFooter" />
+  <div>
+    <h1>Nos Activités</h1>
+    <ul>
+      <li v-for="a in activites" :key="a.id_activite">{{ a.nom_activite }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
-import Navbar from '@/components/Navbar.vue'
-import Footer from '@/components/Footer.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-const route = useRoute()
+// Récupérer l'URL de l'API depuis le .env
+const apiUrl = import.meta.env.VITE_API_URL
+
+const activites = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await axios.get(`${apiUrl}/api/activites`)
+    activites.value = res.data
+  } catch (error) {
+    console.error('Erreur lors du chargement des activités :', error)
+  }
+})
 </script>
-
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
