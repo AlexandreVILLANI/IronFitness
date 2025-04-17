9a6b5b3515c16c2081167812a4e40020fffdf1a8
Alexandre VIILANI <alexandre.villani@edu.univ-fcomte.fr>
2025-04-17 14:47:32 +0200

    ajout d'axios au dépendances du front

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,14 +1,15 @@
-<!-- src/views/Activites.vue -->
 <template>
-  <div>
-    <h1>Nos Activités</h1>
-    <ul>
-      <li v-for="a in activites" :key="a.id_activite">{{ a.nom_activite }}</li>
-    </ul>
+  <div id="app">
+    <Navbar v-if="!route.meta.hideNavbar" />
+    <RouterView />
+    <Footer v-if="!route.meta.hideFooter" />
   </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
@@ -25,7 +26,10 @@ onMounted(async () => {
     console.error('Erreur lors du chargement des activités :', error)
   }
 })
+
+const route = useRoute()
 </script>
+
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
