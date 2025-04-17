5aff0fd11c887321c16b2ef5a0937f0446e1b0bd
Alexandre VIILANI <alexandre.villani@edu.univ-fcomte.fr>
2025-04-17 14:59:32 +0200

    jsp

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    port: 5000, // Ou tout autre port que tu souhaites utiliser
+  },
   base: './',
   plugins: [
     vue(),
